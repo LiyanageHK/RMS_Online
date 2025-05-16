2302bc749de9f4c627ab1df91668aac2cae54037
Hiruni Keshika Liyanage <hiruniliyanage2002@gmail.com>
2025-05-16 20:09:16 +0530

    Fix feedback model fillable attributes and improve code formatting

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
-
+{
     use HasFactory;
 
     protected $fillable = [
@@ -17,6 +17,6 @@ class Feedback extends Model
     ];
 
     protected $table = 'feedback';
-    protected $fillable = ['name', 'feedback'];
+   
 
 }
